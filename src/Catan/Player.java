8b6f1f906655e182f78eb57125b55dc4a2fae1f7
chetmartini <alexmanfred93@gmail.com>
2015-01-05 18:20:06 -0600

    -Changed how the player holds resources: its just a list of resources now.  All methods still work the same though. -Commented everything.

--- src/Catan/Player.java
+++ src/Catan/Player.java
@@ -5,25 +5,21 @@
  */
 package Catan;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 /**
  *
  * @author Alex
  */
 public class Player {
     private String name;
-    private int rocks;
-    private int sheep;
-    private int wood;
-    private int brick;
-    private int wheat;
     private int victorypoints;
     private boolean haslongestroad;
     private boolean haslargestarmy;
     private int remainingroads;
     private int remaininglargesettlements;
     private int remainingsmallsettlements;
-    private int roadsavailable;
     private int smallsettlementcount;
     private int largesettlementcount;
     private int roadcount;
@@ -31,18 +27,15 @@ public class Player {
     private int vpcards;
     private ArrayList<Port> portlist = new ArrayList<>();
     private ArrayList<DC> DChand = new ArrayList<>();
+    private ArrayList<Resource> resourcehand = new ArrayList<>();
     
-    public Player(){
-        rocks = 0;
-        sheep = 0;
-        wood = 0;
-        brick = 0;
-        wheat = 0;
+    
+    public Player(String s){
         smallsettlementcount = 0;
         largesettlementcount = 0;
         roadcount = 0;
         armycount = 0;
-        roadsavailable = 1;
+        name = s;
         vpcards = 0;
         remaininglargesettlements = 5;
         remainingsmallsettlements = 5;
@@ -52,6 +45,12 @@ public class Player {
         haslargestarmy = false;
                 
     }
+
+    /*
+        General methods:
+            int calculateScore() - returns player's victory points.
+    `       String getName() - returns player's name
+    */
     
     public int calculateScore(){
         victorypoints = 0;
@@ -63,120 +62,120 @@ public class Player {
         return victorypoints;       
     }
     
+    public String getName(){
+        return name;
+    }
+
+    /*
+        Resource methods:
+            void addResource(Resource r, int i) - adds given resource to player's resource hand.
+            void removeResource(Resource r, int i) - removes given resource from player's resource hand.
+            void getTotalResourceCount() - returns size of player's resource hand.
+            Resource takeRandomResource() - removes and returns a random resource from player's hand.
+            ~getters for each resource type
+    */    
+    
     public void addResource(Resource r, int i){
-        if(r==Resource.WOOD) wood+=i;
-        if(r==Resource.BRICK) brick+=i;
-        if(r==Resource.ROCK) rocks+=i;
-        if(r==Resource.SHEEP) sheep+=i;
-        if(r==Resource.WHEAT) wheat+=i;
+        if(i>0){
+            for(int n = 0; n<i; n++){
+                resourcehand.add(r);
+            }
+        }
+    }
+    
+    public void removeResource(Resource r, int i){
+        Iterator<Resource> iter = resourcehand.iterator();
+        while (iter.hasNext()) {
+          Resource resource = iter.next();
+            if(resource==r && i>0){
+                iter.remove();
+                i--;
+            }
+        }
     }
     
     public int getTotalResourceCount(){
-        int i = 0;
-        i +=wood;
-        i += brick;
-        i += rocks;
-        i += sheep;
-        i += wheat;
-        return i;
+        return resourcehand.size();
+    }
+    
+    public Resource takeRandomResource(){
+    Random r = new Random();
+    int i = resourcehand.size();
+    int r1;
+    int r2;
+    int n;
+    Resource tmp;
+    for(n=0;n<30;n++){
+        r1 = r.nextInt(i);
+        r2 = r.nextInt(i);
+        tmp = resourcehand.get(r1);
+        resourcehand.set(r1,resourcehand.get(r2));
+        resourcehand.set(r2,tmp);
+    }
+    tmp= resourcehand.get(0);
+    resourcehand.remove(0);
+    return tmp;
     }
     
     public int getRocks(){
-        return rocks;
+        int i = 0;
+        for(Resource r : resourcehand){
+            if(r==Resource.ROCK) i++;
+        }
+        return i;
     }
     
     public int getWood(){
-        return wood;
+        int i = 0;
+        for(Resource r : resourcehand){
+            if(r==Resource.WOOD) i++;
+        }
+        return i;
     }
     
     public int getSheep(){
-        return sheep;
+        int i = 0;
+        for(Resource r : resourcehand){
+            if(r==Resource.SHEEP) i++;
+        }
+        return i;
     }
     
     public int getBrick(){
-        return brick;
+        int i = 0;
+        for(Resource r : resourcehand){
+            if(r==Resource.BRICK) i++;
+        }
+        return i;
     }
     
     public int getWheat(){
-        return wheat;
+        int i = 0;
+        for(Resource r : resourcehand){
+            if(r==Resource.WHEAT) i++;
+        }
+        return i;
     }
+       
+
     
-    public void setRocks(int i){
-        rocks = i;
-    }
-    
-    public void setWood(int i){
-        wood = i;
-    }
-    
-    public void setSheep(int i){
-        sheep = i ;
-    }
-    
-    public void setBrick(int i){
-        brick = i;
-    }
-    
-    public void setWheat(int i){
-        wheat = i;
-    }
-    
-    public int getRoadCount(){
-        return roadcount;
-    }
-    
-    public void setRoadCount(int i){
-        roadcount = i;
-    }
-    
-    public int getSmallSettlementCount(){
-        return smallsettlementcount;
-    }
-    
-    public void setSmallSettlementCount(int i){
-        smallsettlementcount = i;
-    }
-    
-    public int getLargeSettlementCount(){
-        return largesettlementcount;
-    }
-    
-    public void setLargeSettlementCount(int i){
-        largesettlementcount = i;
-    }
-    
-    public void setRemainingSmallSettlement(int i){
-        remainingsmallsettlements = i;
-    }
-    
-    public boolean canMakeSmallSettlement(){
-        if(wheat>0 && wood>0 && brick>0 && sheep>0 && remainingsmallsettlements >0) return true;
-        return false;
-    }
-    
-    public boolean canMakeLargeSettlement(){
-        if(rocks>2 && wheat >1 && remaininglargesettlements >0) return true;
-        return false;
-    }
-    
-    public boolean canMakeRoad(){
-        if(wood>0 && brick>0 && remainingroads >0) return true;
-        return false;
-    }
-    
-    public boolean canMakeDC(){
-        if(sheep>0 && wheat >0 && rocks>0) return true;
-        return false;
-    }
-    
+    /*
+        Settlement methods:
+            void makeSmallSettlement() - removes required resources from player's resource hand and adjusts remaining settlemnt counts for a small settlement.
+            void makeLargeSettlement() - removes required resources from player's resource hand and adjusts remaining settlemnt counts for a large settlement.
+            int getSmallSettlementCount() - returns player's small settlement count.
+            int getLargeSettlementCount() - returns player's large settlement count.
+            boolean canMakeSmallSettlement() - returns whether or not the player can make a small settlement.
+            boolean canMakeLargeSettlement() - returns whether or not the player can make a large settlement.
+    */      
+     
     public void makeSmallSettlement(){
         smallsettlementcount +=1;
         remainingsmallsettlements -= 1;
-        sheep -=1;
-        wheat -=1;
-        brick -=1;
-        wood -=1;
-        this.calculateScore();
+        this.removeResource(Resource.SHEEP, 1);
+        this.removeResource(Resource.WHEAT, 1);
+        this.removeResource(Resource.BRICK, 1);
+        this.removeResource(Resource.WOOD, 1);
     }
     
     public void makeLargeSettlement(){
@@ -184,54 +183,108 @@ public class Player {
         largesettlementcount += 1;
         remainingsmallsettlements +=1;
         remaininglargesettlements -=1;
-        rocks -=3;
-        wheat -=2;
-        this.calculateScore();
+        this.removeResource(Resource.ROCK, 3);
+        this.removeResource(Resource.WHEAT, 2);
     }
     
-    public void makeRoad(){
-        roadcount +=1;
-        remainingroads -=1;
-        wood -=1;
-        brick -=1;
-        this.calculateScore();
+    public int getSmallSettlementCount(){
+        return smallsettlementcount;
     }
     
-    public void makeDC(){
-        //take card from dc deck
-        //if the card is a vp point card increment vp card score
-        sheep -=1;
-        rocks -=1;
-        wheat -=1;
-        this.calculateScore();
+    public int getLargeSettlementCount(){
+        return largesettlementcount;
+    }    
+    
+    public boolean canMakeSmallSettlement(){
+        if(this.getBrick()>0 && this.getWood()>0 && this.getSheep()>0 && this.getWheat()>0 && remainingsmallsettlements >0) return true;
+        return false;
     }
     
-    public void addArmy(){
-        armycount++;
+    public boolean canMakeLargeSettlement(){
+        if(this.getRocks()>2 && this.getWheat() >1 && remaininglargesettlements >0) return true;
+        return false;
     }
+    
+    
+    /*
+    Road methods:
+        void setHasLongestRoad(boolean b) - sets whether or not the player has the longest road.
+        void makeRoad() - removes required resources from player's resource hand and decrements remaining roads.
+        boolean canMakeRoad() - returns whether or not the player has required resources and remaining roads to build a road.
+        int getRoadCount() - returns length of player's longest road.
+        void setRoadCount(int i) - sets length of player's longest road.
+    */
     
     public void setHasLongestRoad(boolean b){
         haslongestroad=b; 
     }
+
+    public void makeRoad(){
+        remainingroads--;
+        this.removeResource(Resource.WOOD, 1);
+        this.removeResource(Resource.BRICK, 1);
+    }
+    
+    public boolean canMakeRoad(){
+        if(this.getWood()>0 && this.getBrick()>0 && remainingroads >0) return true;
+        return false;
+    }
+
+    public int getRoadCount(){
+        return roadcount;
+    }
+    
+    public void setRoadCount(int i){
+        roadcount = i;
+    }   
+   
+    
+    /*   
+    Army methods:
+        void addArmy(int i) - adds armies to the player's army count.
+        void setHasLargestArmy(boolean b) - changes haslargestarmy field.
+        int getArmyCount() - returns the player's army count 
+    */
+
+    public void addArmy(int i){
+        armycount += i;
+    }    
     
     public void setHasLargestArmy(boolean b){
         haslargestarmy = b;
+        this.calculateScore();
+    }
+    
+    public int getArmyCount(){
+        return armycount;
+    }
+    
+    
+    
+    /*   
+    DC methods:
+        void makeDC(DC dc) - takes resources out of player's resource hand and adds a DC to the player's DC hand and increments vpcount if it is a VP card.
+        List<DC> getDCHand() - returns a list of all the player's DCs.
+        boolean canMakeDC() - returns whether or not the player has the required resources to make a DC.
+    */
+    
+    public void makeDC(DC dc){
+        //take card from dc deck
+        //if the card is a vp point card increment vp card score
+        this.removeResource(Resource.ROCK, 1);
+        this.removeResource(Resource.SHEEP, 1);
+        this.removeResource(Resource.WHEAT, 1);
+        DChand.add(dc);
+        if(dc==DC.VP) vpcards++;
     }
     
     public List<DC> getDCHand(){
         return DChand;
     }
-    
-    public void giveDC(DC dc){
-        DChand.add(dc);
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getName() {
-        return name;
-    }
+    public boolean canMakeDC(){
+        if(this.getSheep()>0 && this.getWheat() >0 && this.getRocks()>0) return true;
+        return false;
+    }   
     
 }
